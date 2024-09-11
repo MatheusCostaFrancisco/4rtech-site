afea50cc94fb2f0ad3956565e3778cbfb3b9da03
MatheusCostaFrancisco <contato.matheuscf@gmail.com>
2024-09-10 21:25:15 -0300

    AOS

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+// src/global.d.ts or src/types/aos.d.ts
+declare module "aos" {
+  const AOS: any // Use any type if you want to disable type checking
+  export default AOS
+}
